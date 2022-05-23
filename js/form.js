38b797ac4265a007e389e23a9c053e0a81c51400
Diogo Santana <102337705+Dhi-Carvalho-DEV@users.noreply.github.com>
2022-05-23 20:32:38 -0300

    Criação das validações para caso os campos não sejam preenchidos

--- js/form.js
+++ js/form.js
@@ -3,20 +3,13 @@ var botaoAdicionar = document.querySelector('#adicionar-paciente')
 botaoAdicionar.addEventListener('click', function (event) {
   event.preventDefault()
   var form = document.querySelector('#form-adiciona')
-
   var paciente = obtemDadosPaciente(form)
-
-  if (!validaPaciente(paciente)) {
-    console.log('Paciente inválido')
-    return
-  }
-
   var pacienteTr = montarTr(paciente)
+
   var erros = validaPaciente(paciente)
 
   if (erros.length > 0) {
-    var mensagemErro = document.querySelector("#mensagem-erro")
-    mensagemErro.textContent = erros
+    exibeMsgDeErro(erros)
     return
   }
 
@@ -24,7 +17,20 @@ botaoAdicionar.addEventListener('click', function (event) {
   tabela.appendChild(pacienteTr)
 
   form.reset()
+
+  var mensagensErro = document.querySelector('#mensagens-erro')
+  mensagensErro.innerHTML = ''
 })
+
+function exibeMsgDeErro(erros) {
+  var ul = document.querySelector('#mensagens-erro')
+  ul.innerHTML = ''
+  erros.forEach(function (erro) {
+    var li = document.createElement('li')
+    li.textContent = erro
+    ul.appendChild(li)
+  })
+}
 
 function obtemDadosPaciente(form) {
   var paciente = {
@@ -62,12 +68,28 @@ function montarTd(dado, classe) {
 function validaPaciente(paciente) {
   var erros = []
 
+  if (paciente.nome.length == 0) {
+    erros.push('O nome não pode ser em branco')
+  }
+
+  if (paciente.peso.length == 0) {
+    erros.push('O peso não pode ser em branco')
+  }
+
+  if (paciente.altura.length == 0) {
+    erros.push('O altura não pode ser em branco')
+  }
+
+  if (paciente.gordura.length == 0) {
+    erros.push('O gordura não pode ser em branco')
+  }
+
   if (!validaPeso(paciente.peso)) {
     erros.push('O peso é inválido!')
   }
 
   if (!validaAltura(paciente.altura)) {
-    erros.push("A altura é inválida!")
+    erros.push('A altura é inválida!')
   }
 
   return erros
